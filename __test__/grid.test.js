b127d599a12d51382812a514d4b64e8b6425e5d6
Gerald Isabelle <info@geraldisabelle.me>
2017-11-01 00:47:54 +0000

    add createGrid functionality

--- __test__/grid.test.js
+++ __test__/grid.test.js
@@ -3,12 +3,23 @@ import { createGrid } from '../grid'
 
 describe('Grid', () => {
 
-    it('Create a (3,2) grid ', () => {
-        expect(createGrid(3,3)).toBe([]);
+    it('Create a (5,3) grid ', () => {
+        expect(createGrid(5,3)).toEqual([
+            ['x','x','x','x','x'],
+            ['x','x','x','x','x'],
+            ['x','x','x','x','x']
+        ]);
     });
 
     it('Create a (5,6) grid ', () => {
-        expect(createGrid(5,6)).toBe([]);
+        expect(createGrid(5,6)).toEqual([
+            ['x','x','x','x','x'],
+            ['x','x','x','x','x'],
+            ['x','x','x','x','x'],
+            ['x','x','x','x','x'],
+            ['x','x','x','x','x'],
+            ['x','x','x','x','x']
+        ]);
     });
 
 });
--- grid.js
+++ grid.js
@@ -1,3 +1,12 @@
 export const createGrid = (x, y) => {
 
+    let grid = [];
+
+    for (let i = 0; i < y; i++) {
+        grid.push([]);
+        for(let r = 0; r < x; r++) {
+            grid[i].push('x')
+        }
+    }
+    return grid;
 };
